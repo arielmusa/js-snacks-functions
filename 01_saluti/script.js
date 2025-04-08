aa4bef4f84fcce58128e70cc76bf9059d06db909
arielmusa <arielmusainfo@gmail.com>
2025-04-08 15:11:07 +0200

    snack 01 with arrow function

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,9 +4,10 @@
 const userName = "Mario";
 
 // Dichiara la funzione qui.
-function greetUser(name) {
+/* function greetUser(name) {
   return `Ciao ${name}`;
-}
+} */
+const greetUser = (name) => `Ciao ${name}`;
 // Invoca la funzione qui e stampa il risultato in console
 console.log(greetUser(userName));
 //Risultato atteso se si passa 'Mario': // ciao Mario
