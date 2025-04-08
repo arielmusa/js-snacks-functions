2e4388b8db9c2169a40c6db22794a68b91a4ccbe
arielmusa <arielmusainfo@gmail.com>
2025-04-08 15:30:00 +0200

    04 snack

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,26 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa 
+e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
-
+function countVowels(string) {
+  string = string.toLowerCase();
+  let vowelsCount = 0;
+  for (let i = 0; i < string.length; i++) {
+    currentChar = string[i];
+    if (
+      currentChar === "a" ||
+      currentChar === "e" ||
+      currentChar === "i" ||
+      currentChar === "o" ||
+      currentChar === "u"
+    )
+      vowelsCount += 1;
+  }
+  return vowelsCount;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(countVowels(word));
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
