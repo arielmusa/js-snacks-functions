ae7220bb1a95814ae18d17b1323d695be1aca0cf
arielmusa <arielmusainfo@gmail.com>
2025-04-08 15:19:00 +0200

    snack 02: use arrow function

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,14 +4,22 @@ e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito *
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
-function firstCharArray(array) {
+/* function firstCharArray(array) {
   const newArray = [];
   for (let i = 0; i < array.length; i++) {
     currentItem = array[i];
     newArray.push(currentItem[0]);
   }
   return newArray;
-}
+} */
+const firstCharArray = (array) => {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    currentItem = array[i];
+    newArray.push(currentItem[0]);
+  }
+  return newArray;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 console.log(firstCharArray(names));
