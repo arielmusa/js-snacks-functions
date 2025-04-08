b847194475f858c7dc212b077fe0943dafbe61dd
arielmusa <arielmusainfo@gmail.com>
2025-04-08 15:25:34 +0200

    03 snack: use arrow function

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,14 +4,22 @@ e una lettera e restituisca un array contenente solo le parole che iniziano con 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
-function filterArray(array, firstChar) {
+/* function filterArray(array, firstChar) {
   newArray = [];
   for (let i = 0; i < array.length; i++) {
     currentItem = array[i];
     if (currentItem[0] === firstChar) newArray.push(currentItem);
   }
   return newArray;
-}
+} */
+const filterArray = (array, firstChar) => {
+  newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    currentItem = array[i];
+    if (currentItem[0] === firstChar) newArray.push(currentItem);
+  }
+  return newArray;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 console.log(filterArray(names, "A"));
