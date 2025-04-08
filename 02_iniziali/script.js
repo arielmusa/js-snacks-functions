287d45a9337f154fae67671634e0b33c53abb0f5
arielmusa <arielmusainfo@gmail.com>
2025-04-08 15:16:48 +0200

    02 snack

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,18 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi 
+e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function filterArray(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    currentItem = array[i];
+    newArray.push(currentItem[0]);
+  }
+  return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(filterArray(names));
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
